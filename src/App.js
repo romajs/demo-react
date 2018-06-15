9cc4d60304b9d539f27b001cb0c486d0ecb847ac
romajs <josers@ciandt.com>
2018-06-14 21:15:42 -0300

    Added TimerInSeconds component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css'
 import HelloMessage from './components/HelloMessage'
 import logo from './logo.svg'
 import React, { Component } from 'react'
+import TimerInSeconds from './components/TimerInSeconds'
 
 class App extends Component {
   render () {
     return [
       <HelloMessage key={0} name='world' />,
-      <logo key={1} />
+      <logo key={1} />,
+      <TimerInSeconds key={3} />
     ]
   }
 }

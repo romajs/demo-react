ca21112b34a1ffdc47293548a0bdc8021f3d9c6a
josers-ciandt <josers@ciandt.com>
2018-08-01 01:00:43 -0300

    Added TodoApp and MarkdownEditor

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import HelloMessage from './components/HelloMessage'
 import logo from './logo.svg'
 import React, { Component } from 'react'
 import TimerInSeconds from './components/TimerInSeconds'
+import TodoApp from './components/TodoApp'
+import MarkdownEditor from './MarkdownEditor'
 
 class App extends Component {
   render () {
     return [
       <HelloMessage key={0} name='world' />,
       <logo key={1} />,
-      <TimerInSeconds key={3} />
+      <TimerInSeconds key={3} />,
+      <TodoApp key={4} />,
+      <MarkdownEditor key={5} />
     ]
   }
 }

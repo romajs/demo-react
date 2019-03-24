4106748fa7083e3c12876bf756216340fa48d38b
romajs <romajslit@gmail.com>
2019-03-24 04:06:10 -0300

    Dynamic routes

--- src/features/application/App.js
+++ src/features/application/App.js
@@ -1,20 +1,12 @@
 import { ConnectedRouter } from 'connected-react-router'
-import { Redirect, Route, Switch } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import React from 'react'
 import ReactRouterPropTypes from 'react-router-prop-types'
 
-import { BarPage } from '../bar/BarPage'
-import { BazPage } from '../baz/BazPage'
 import { FullLoading } from '../misc/loading/FullLoading'
-import { HomePage } from '../home/HomePage'
-import { LoginPageContainer } from '../auth/LoginPageContainer'
 import { PageContainer } from './page/PageContainer'
-import { PrivateBarPage } from '../privateBar/PrivateBarPage'
-import { PrivateBazPage } from '../privateBaz/PrivateBazPage'
-import { PrivateHomePageContainer } from '../privateHome/PrivateHomePageContainer'
-import { PrivateRoute } from '../auth/PrivateRoute'
-import * as pages from '../../pages'
+import { routes } from './routes'
 
 export const App = ({ history, isAuthenticated, loading }) => (
   <React.Fragment>
@@ -22,29 +14,12 @@ export const App = ({ history, isAuthenticated, loading }) => (
     <PageContainer push={history.push} />
     <ConnectedRouter history={history}>
       <Switch>
-        <Redirect exact from={pages.ROOT_PATH} to={pages.HOME.url} />
-        <Route exact path={pages.BAR.url} component={BarPage} />
-        <Route exact path={pages.BAZ.url} component={BazPage} />
-        <Route exact path={pages.HOME.url} component={HomePage} />
-        <Route exact path={pages.LOGIN.url} component={LoginPageContainer} />
-        <PrivateRoute
-          exact
-          path={pages.PRIVATE_BAR.url}
-          component={PrivateBarPage}
-          isAuthenticated={isAuthenticated}
-        />
-        <PrivateRoute
-          exact
-          path={pages.PRIVATE_BAZ.url}
-          component={PrivateBazPage}
-          isAuthenticated={isAuthenticated}
-        />
-        <PrivateRoute
-          exact
-          path={pages.PRIVATE_HOME.url}
-          component={PrivateHomePageContainer}
-          isAuthenticated={isAuthenticated}
-        />
+        {routes.map(([Component, props]) => (
+          <Component
+            isAuthenticated={isAuthenticated}
+            {...props}
+          />
+        ))}
       </Switch>
     </ConnectedRouter>
   </React.Fragment>
